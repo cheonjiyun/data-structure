089e6ccbabe67a36f60eb88322b6ba5e437ce8d8
gunbambam <gunbam715@gmail.com>
2023-03-20 23:00:27 +0900

    2week 실습5 리스트 두개 사용

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ typedef struct {
 
 
 elem_t get_next() { // int
-	int n;
-	scanf_s("%d", &n);
+	int n = rand() % 100;
+	//scanf_s("%d", &n);
 	return n;
 }
 
@@ -43,12 +43,7 @@ void free_elem() {
 //	free(e);
 //}
 
-void input_list(arrlist_t* lp, int size) {
-	lp->size = size;
-	for (int i = 0; i < lp -> size; ++i) {
-		lp -> array[i] = get_next();
-	}
-}
+
 
 int compare(elem_t e1, elem_t e2) {
 	return e1 - e2;
@@ -88,13 +83,6 @@ elem_t delete_list(arrlist_t* lp, int pos) {
 	return result;
 }
 
-int find(arrlist_t* lp, elem_t val) {
-	for (int i = 0; i < lp -> size; i++) {
-		if (compare(lp -> array[i], val) == 0)
-			return i; // 찾으면 인덱스 반환
-	}
-	return -1; // 못찾으면 -1
-}
 
 elem_t update_list(arrlist_t* lp, int pos, elem_t val) {
 	elem_t result = lp -> array[pos];
@@ -103,74 +91,128 @@ elem_t update_list(arrlist_t* lp, int pos, elem_t val) {
 	return result;
 }
 
+elem_t get(arrlist_t* lp, int index) {
+	return lp->array[index];
+}
+
+
+int find(arrlist_t* lp, elem_t val) {
+	for (int i = 0; i < lp->size; i++) {
+		if (compare(lp->array[i], val) == 0)
+			return i; // 찾으면 인덱스 반환
+	}
+	return -1; // 못찾으면 -1
+}
+
+
+int len(arrlist_t* lp) {
+	return lp->size;
+}
+
+
+
+void init_list(arrlist_t* lp) {
+	lp->size = 0;
+}
+
 void clear(arrlist_t* lp) {
 	for (int i = 0; i < lp->size; i++) {
-		free_elem(lp->array[i]);
+		free(lp->array[i]);
+		lp->size = 0;
 	}
-	lp->size = 0;
+}
+
+
+void input_list(arrlist_t* lp, int size) {
+	lp->size = size;
+	for (int i = 0; i < lp->size; ++i) {
+		lp->array[i] = get_next();
+	}
 }
 
 void free_list(arrlist_t* lp) {
 	clear(lp);
 }
 
-void run_menu(arrlist_t* lp) {
-	int pos;
-	elem_t val;
-	int menu;
-	while (1) {
-		printf("\n(1) 끝에 값 추가 \n(2) pos번째에 값 추가\n");
-		printf("(3) pos번째 삭제 \n(4) pos번째 값 변경 \n(5) val값 검색 \n(6)종료");
-		printf(" 메뉴선택 => ");
-		scanf_s("%d", &menu);
-		if (menu >= 6) break;
-		switch (menu) {
-		case 1:
-			printf("끝에 추가할 값: ");
-			val = get_next();
-			add_list(lp, val);
-			break;
-		case 2:
-			printf("추가할 위치와 값: ");
-			scanf_s("%d", &pos);
-			insert_list(lp, pos, get_next());
-			break;
-		case 3:
-			printf("삭제할 위치: ");
-			scanf_s("%d", &pos);
-			elem_t val = delete_list(lp, pos);
-			free_elem(val);
-			break;
-		case 4:
-			printf("변경할 위치와 값: ");
-			scanf_s("%d", &pos);
-			val = update_list(lp, pos, get_next());
-			free_elem(val);
-			break;
-		case 5:
-			printf("찾으려는 값 : ");
-			val = get_next();
-			pos = find(lp, val);
-			if (pos == -1)
-				printf("%s는 없는 값입니다\n", str(val));
-			else
-				printf("%s는[%d]에 있습니다\n", str(val), pos);
-			break;
-		default:
-			break;
-		}
-		print_list(lp);
-	}
-	free_list(lp);
-}
+//
+//
+//void run_menu(arrlist_t* lp) {
+//	int pos;
+//	elem_t val;
+//	int menu;
+//	while (1) {
+//		printf("\n(1) 끝에 값 추가 \n(2) pos번째에 값 추가\n");
+//		printf("(3) pos번째 삭제 \n(4) pos번째 값 변경 \n(5) val값 검색 \n(6)종료");
+//		printf(" 메뉴선택 => ");
+//		scanf_s("%d", &menu);
+//		if (menu >= 6) break;
+//		switch (menu) {
+//		case 1:
+//			printf("끝에 추가할 값: ");
+//			val = get_next();
+//			add_list(lp, val);
+//			break;
+//		case 2:
+//			printf("추가할 위치와 값: ");
+//			scanf_s("%d", &pos);
+//			insert_list(lp, pos, get_next());
+//			break;
+//		case 3:
+//			printf("삭제할 위치: ");
+//			scanf_s("%d", &pos);
+//			elem_t val = delete_list(lp, pos);
+//			free_elem(val);
+//			break;
+//		case 4:
+//			printf("변경할 위치와 값: ");
+//			scanf_s("%d", &pos);
+//			val = update_list(lp, pos, get_next());
+//			free_elem(val);
+//			break;
+//		case 5:
+//			printf("찾으려는 값 : ");
+//			val = get_next();
+//			pos = find(lp, val);
+//			if (pos == -1)
+//				printf("%s는 없는 값입니다\n", str(val));
+//			else
+//				printf("%s는[%d]에 있습니다\n", str(val), pos);
+//			break;
+//		default:
+//			break;
+//		}
+//		print_list(lp);
+//	}
+//	free_list(lp);
+//}
 
 
 void main() {
-	arrlist_t mylist;
-	int size;
-	printf("문자의 개수를 입력하시오 : ");
-	scanf_s("%d", &size);
-	input_list(&mylist, size);
-	run_menu(&mylist);
-	printf("\n안녕히 가세요\n");
+	int num;
+	srand(1);
+	arrlist_t org_list, new_list;
+	init_list(&org_list);
+	input_list(&org_list, 100);
+	printf("original list : \n"); print_list(&org_list);
+	printf("\n");
+	init_list(&new_list, 0);
+	for (int i = len(&org_list) - 1; i >= 0; i--) {
+		num = get(&org_list, i);
+		if (num > 50) {
+			num = delete_list(&org_list, i);
+			add_list(&new_list, num);
+		}
+	}
+	printf("50 이하 list : \n"); print_list(&org_list);
+	printf("\n");
+	printf("50 초과 list : \n"); print_list(&new_list);
+
+
+	//arrlist_t mylist;
+	//int size;
+	//printf("문자의 개수를 입력하시오 : ");
+	//scanf_s("%d", &size);
+	//input_list(&mylist, size);
+	//run_menu(&mylist);
+	//printf("\n안녕히 가세요\n");
 }
