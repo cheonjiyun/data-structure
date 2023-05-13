b9f5d2f8b35e416f0dc0e08691d80041f1be4958
CheonJiyun <70828192+gunbambam@users.noreply.github.com>
2023-05-13 22:29:47 +0900

    Add files via upload

---
+++ 10 week - tree/7 이진트리 - 탐색연산 - 반복방법/tree.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#ifndef max
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#endif // !max
+
+
+typedef struct TreeNode {
+	int key;
+	struct TreeNode* left, * right;
+} TreeNode;
+
+TreeNode* search(TreeNode* node, int key) {
+	while (node != NULL) {
+		if (key == node->key) return node->key;
+		else if (key < node->key) { //작으면 외쪽 트리로 내려감
+			return search(node->left, key);
+		}
+		else {
+			return search(node->right, key);
+		}
+	}
+	return NULL; // 없으면 null
+}
+
+int main(void) {
+
+	TreeNode n4 = { 25, NULL, NULL };
+	TreeNode n5 = { 31, NULL, NULL };
+	TreeNode n3 = { 26, &n4, &n5 };
+	TreeNode n2 = { 7, NULL, NULL };
+	TreeNode n1 = { 18, &n2, &n3 };
+
+	printf("탐색결과 = %d \n", search(&n1, 25));
+	return 0;
+}
